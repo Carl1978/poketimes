6a71e84e479c25f7894a779efcc50f881123a43e
Carl Phillips <carl.phillips@gmail.com>
2018-10-07 21:54:48 +0100

    test: some comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
+// RESTful API
 //import axios from "axios";
 import { connect } from "react-redux";
 import { deletePost } from "../actions/postActions";
